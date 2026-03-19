90a39ca2a7648d232be6cd3f5655422c0511096c
Damandundel <damian.georgiev08@gmail.com>
2026-03-19 11:44:28 +0200

    Simplify HomeController, add house listing feature
    Removed Privacy page and links. Updated layout branding and footer. Added HouseController with AllHouses and Details actions, HouseViewModel, and views for listing and viewing house details.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,24 +1,13 @@
 using System.Diagnostics;
 using HouseRentingSystem.Models;
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace HouseRentingSystem.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
